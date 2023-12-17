d9926e41ce4d3a3827aa2c109c54647cf2a89e5f
Boris Bera <beraboris@gmail.com>
2023-12-17 09:01:50 -0500

    Switch VM to boot through EFI

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,11 +1,30 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.nixos-generators.nixosModules.vm
     # inputs.nixos-generators.nixosModules.vm-nogui
   ];
 
   virtualisation.useBootLoader = true;
-  # virtualisation.useEFIBoot = true;
+  virtualisation.useEFIBoot = true;
+
+  boot.loader = {
+    # EFI Boot
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    # Use systemd boot
+    grub.enable = false;
+    systemd-boot.enable = true;
+
+    # Skip the menu
+    timeout = 0;
+  };
 
   virtualisation.forwardPorts = [
     {
--- hosts/vm/hardward-configuration.nix
+++ hosts/vm/hardward-configuration.nix
@@ -5,14 +5,17 @@
 }: {
   imports = [];
 
-  boot.initrd.availableKernelModules = ["ata_piix" "xhci_pci" "sd_mod" "sr_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
-  boot.extraModulePackages = [];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
+  # When Nix builds a VM, it uses well-defined labels, we reuse those. 
+  # See: https://github.com/NixOS/nixpkgs/blob/4c501306af1ab6c19491fdafebb30fd097eb42c5/nixos/modules/virtualisation/qemu-vm.nix#L264-L267
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/EFI";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [];
