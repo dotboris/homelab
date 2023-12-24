1491cce8a9c564d6d8c3eb06dd10a7d272e2ec94
Boris Bera <beraboris@gmail.com>
2023-12-24 09:40:31 -0500

    Enable anonymous login for grafana

--- modules/monitoring/grafana.nix
+++ modules/monitoring/grafana.nix
@@ -4,10 +4,31 @@
 in {
   services.grafana = {
     enable = true;
-    settings.server = {
-      http_addr = "127.0.0.1";
-      http_port = httpPort;
-      domain = httpHost;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = httpPort;
+        domain = httpHost;
+      };
+
+      users = {
+        allow_sign_up = false;
+        allow_org_create = false;
+      };
+      "auth.anonymous" = {
+        enabled = true;
+        hide_version = true;
+      };
+
+      analytics = {
+        # We manage the software in nix, this is just noise
+        check_for_updates = false;
+        check_for_plugin_updates = false;
+
+        # Stop it from phoning home
+        feedback_links_enabled = false;
+        reporting_enabled = false;
+      };
     };
   };
 
