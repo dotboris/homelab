472c613f30721ba62e7479bd78d5228a542b4c8a
Boris Bera <beraboris@gmail.com>
2024-08-28 10:33:13 -0400

    Ship netdata alerts to ntfy

--- modules/monitoring/netdata.nix
+++ modules/monitoring/netdata.nix
@@ -1,10 +1,11 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
+  inherit (config.homelab.reverseProxy) vhosts;
   cfg = config.homelab.monitoring.netdata;
-  vhost = config.homelab.reverseProxy.vhosts.netdata;
 in {
   options.homelab.monitoring.netdata = {
     port = lib.mkOption {
@@ -25,13 +26,20 @@ in {
         db.mode = "dbengine";
       };
 
+      configDir = {
+        "health_alarm_notify.conf" = pkgs.writeText "health_alarm_notify.conf" ''
+          SEND_NTFY=YES
+          DEFAULT_RECIPIENT_NTFY=https://${vhosts.ntfy.fqdn}/netdata
+        '';
+      };
+
       enableAnalyticsReporting = false;
     };
 
     homelab.reverseProxy.vhosts.netdata = {};
     services.traefik.dynamicConfigOptions.http = {
       routers.netdata = {
-        rule = "Host(`${vhost.fqdn}`)";
+        rule = "Host(`${vhosts.netdata.fqdn}`)";
         service = "netdata";
         tls = config.homelab.reverseProxy.tls.value;
       };
