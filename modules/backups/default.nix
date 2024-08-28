083aff2b6ba67e0c6ae67c7fc8971555929ea88b
Boris Bera <beraboris@gmail.com>
2024-08-28 12:07:46 -0400

    Setup backup notifications

--- modules/backups/default.nix
+++ modules/backups/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -7,6 +8,20 @@ with lib; let
   cfg = config.homelab.backups;
   autoresticCfg = config.services.autorestic;
   backendKeys = builtins.attrNames autoresticCfg.settings.backends;
+
+  ntfyTopic = "https://${config.homelab.reverseProxy.vhosts.ntfy.fqdn}/backups";
+  notifySuccess = pkgs.writeShellScript "notify-backup-success.sh" ''
+    ${pkgs.curl}/bin/curl -s \
+      -H "Title: Backup Complete" \
+      -H "Priority: low" \
+      -d "Backup of location $AUTORESTIC_LOCATION has completed sucessfully." \
+      ${ntfyTopic}'';
+  notifyFailure = pkgs.writeShellScript "notify-backup-failure.sh" ''
+    ${pkgs.curl}/bin/curl -s \
+      -H "Title: Backup Failed" \
+      -H "Priority: high" \
+      -d "Backup of location $AUTORESTIC_LOCATION has failed." \
+      ${ntfyTopic}'';
 in {
   imports = [
     ./backends/local.nix
@@ -41,13 +56,20 @@ in {
           keep-monthly = 12;
           keep-yearly = 7;
         };
-        locations = mapAttrs (_: value:
-          value
-          // {
-            to = backendKeys;
-            forget = "yes";
-          })
-        cfg.locations;
+        locations =
+          mapAttrs (
+            _: value:
+              recursiveUpdate {
+                to = backendKeys;
+                forget = "yes";
+                hooks = {
+                  success = ["${notifySuccess}"];
+                  failure = ["${notifyFailure}"];
+                };
+              }
+              value
+          )
+          cfg.locations;
       };
     };
 
