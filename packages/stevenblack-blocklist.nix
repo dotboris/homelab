bd3be1a55d6ea66aa9d655dd8256f06b0286aab8
Boris Bera <beraboris@gmail.com>
2024-10-24 19:20:53 -0400

    Update blocklist

--- packages/stevenblack-blocklist.nix
+++ packages/stevenblack-blocklist.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "stevenblack-blocklist";
-  version = "3.14.125";
+  version = "3.14.126";
   # https://github.com/StevenBlack/hosts/releases
   src = pkgs.fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    sha256 = "sha256-6bZhQRCGAeBzOXF8CRFDDG9fI0szycsR/6XDoFaYAjs=";
+    sha256 = "sha256-t5kzFO1yjY7RrFLeK9MBbY7aRb2ThY1ytaXGVcC0Y+g=";
   };
   installPhase = ''
     mkdir -p $out
