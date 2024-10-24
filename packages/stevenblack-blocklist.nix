c5dc15d0fd559d0b1c7ad88bc1d9f47dfe0b72e0
Boris Bera <beraboris@gmail.com>
2024-10-24 18:58:30 -0400

    Build proper package for blocklist

--- packages/stevenblack-blocklist.nix
+++ packages/stevenblack-blocklist.nix
@@ -1,11 +1,20 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  stdenv,
+  ...
+}:
+stdenv.mkDerivation rec {
+  pname = "stevenblack-blocklist";
   version = "3.14.125";
-in
   # https://github.com/StevenBlack/hosts/releases
-  pkgs.fetchFromGitHub {
-    name = "stevenback-blocklist-${version}";
+  src = pkgs.fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
     sha256 = "sha256-6bZhQRCGAeBzOXF8CRFDDG9fI0szycsR/6XDoFaYAjs=";
-  }
+  };
+  installPhase = ''
+    mkdir -p $out
+    cp hosts $out/hosts
+  '';
+}
