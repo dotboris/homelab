9ea55ee01a47d8804a08704f28fc93a6de4d1665
Boris Bera <beraboris@gmail.com>
2024-10-24 19:02:36 -0400

    Convert whitelist to proper package

--- packages/anudeepnd-allowlist.nix
+++ packages/anudeepnd-allowlist.nix
@@ -1,11 +1,19 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  stdenv,
+  ...
+}:
+stdenv.mkDerivation rec {
+  pname = "anudeepnd-allowlist";
   version = "2.0.1";
-in
-  # https://github.com/anudeepND/whitelist/releases
-  pkgs.fetchFromGitHub {
-    name = "anudeepnd-allowlist-${version}";
+  src = pkgs.fetchFromGitHub {
     owner = "anudeepND";
     repo = "whitelist";
     rev = "v${version}";
     sha256 = "sha256-TWtYNxMU5gpe5Y4Th6tQaiOA09DBV7iJFPr9P7CAfag=";
-  }
+  };
+  installPhase = ''
+    mkdir -p $out/domains
+    cp domains/whitelist.txt $out/domains/whitelist.txt
+  '';
+}
