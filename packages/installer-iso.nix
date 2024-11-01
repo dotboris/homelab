d1d25aba12f6703ebd79765ca8a02358893a269b
Boris Bera <beraboris@gmail.com>
2024-11-01 14:44:51 -0400

    Fix installer-iso failing to build

--- packages/installer-iso.nix
+++ packages/installer-iso.nix
@@ -12,8 +12,14 @@
     users.users.root.openssh.authorizedKeys.keys = [consts.dotborisSshPubkey];
 
     environment.systemPackages = [
+      # Image alrady ships disko but it's the nipkgs version not the one from
+      # the flake. This ensures that we use the same version everywhere.
       inputs.disko.packages.${system}.disko
     ];
+
+    # Image enables bcachefs by default but it causes an assertion error. Not
+    # sure why honestly but I don't think that we use bcachefs at all.
+    boot.supportedFilesystems.bcachefs = false;
 
     nix.settings = {
       experimental-features = ["nix-command" "flakes"];
