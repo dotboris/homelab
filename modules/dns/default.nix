8a10c5f909b5c79b28c8445229664bd8c4114ba8
Boris Bera <beraboris@gmail.com>
2024-12-21 16:43:52 -0500

    fix(dns): add trailing `.` to cloud alias

--- modules/dns/default.nix
+++ modules/dns/default.nix
@@ -27,7 +27,7 @@
       aliases = [
         "home.dotboris.io."
         "archive.dotboris.io."
-        "cloud.dotboris.io"
+        "cloud.dotboris.io."
         "feeds.dotboris.io."
         "netdata.dotboris.io."
         "traefik.dotboris.io."
@@ -43,7 +43,7 @@
       aliases = [
         "home-test.dotboris.io."
         "archive-test.dotboris.io."
-        "cloud-test.dotboris.io"
+        "cloud-test.dotboris.io."
         "feeds-test.dotboris.io."
         "netdata-test.dotboris.io."
         "traefik-test.dotboris.io."
