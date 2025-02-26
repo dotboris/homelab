3fc7ea1f23621f38e84398877ccf1ab05b1466cf
Boris Bera <beraboris@gmail.com>
2025-02-25 20:27:53 -0500

    fix(admin)!: drop old ssh key

--- consts.nix
+++ consts.nix
@@ -1,8 +1,5 @@
 {
   dotboris.ssh.pubKeys = [
-    # old
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOrL2lDUwOQ9K98de3YQqscdLAHqoZJCuCocL6TZYZq"
-    # new
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSVAavmM8Tz004E0GYZBerdouuquNldm8yozz76ZUw8"
   ];
 }
