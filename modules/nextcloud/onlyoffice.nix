50da937f63a2b2a5064c2e409c66cf0c819a018c
Boris Bera <beraboris@gmail.com>
2025-03-09 20:30:47 -0400

    feat(nextcloud): WIP: setup onlyoffice integration
    This doesn't quite work yet because the traefik -> nginx -> onlyoffice
    setup generates a bad redirect to the nginx port.

--- modules/nextcloud/onlyoffice.nix
+++ modules/nextcloud/onlyoffice.nix
@@ -1,26 +1,43 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: let
   inherit (lib) types mkEnableOption mkIf mkOption;
   cfg = config.homelab.nextcloud.onlyoffice;
-  nextcloudCfg = config.homelab.nextcloud;
   vhost = config.homelab.reverseProxy.vhosts.office;
 in {
   options.homelab.nextcloud.onlyoffice = {
     enable = mkEnableOption "NextCloud OnlyOffice integration";
     port = mkOption {type = types.port;};
+    # TODO: the traefik -> nginx -> onlyoffice setup breaks. Nextcloud fails to
+    #   embed to render the document through an iframe because it gets rewritten
+    #   to https://{fqdn}:{nginxPort}/ for some reason. Also, HTTPS keeps
+    #   getting dropped in those redirects.
     nginxPort = mkOption {type = types.port;};
   };
   config = mkIf cfg.enable {
+    # TODO: find a way to pass secret to nextcloud directly instead of having to
+    #   set it by hand. Could be doable with occ command.
+    sops.secrets."nextcloud/onlyoffice/jwt-secret" = {
+      owner = "onlyoffice";
+    };
     homelab.reverseProxy.vhosts.office = {};
     services = {
       onlyoffice = {
         inherit (cfg) port;
         enable = true;
         hostname = vhost.fqdn;
+        jwtSecretFile = config.sops.secrets."nextcloud/onlyoffice/jwt-secret".path;
+      };
+      nextcloud = {
+        settings.onlyoffice.DocumentServerUrl = "https://${vhost.fqdn}";
+        extraApps = {
+          inherit
+            (config.services.nextcloud.package.packages.apps)
+            onlyoffice
+            ;
+        };
       };
       nginx.virtualHosts.${vhost.fqdn}.listen = [
         # onlyoffice uses nginx under the hood. We're using traefik. This moves
