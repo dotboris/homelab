e7b1050c878d9c10fa296a6d84234ab9ff75140c
Boris Bera <beraboris@gmail.com>
2025-05-03 12:44:50 -0400

    fix: update

--- packages/stevenblack-blocklist.nix
+++ packages/stevenblack-blocklist.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "stevenblack-blocklist";
-  version = "3.15.29";
+  version = "3.15.36";
   # https://github.com/StevenBlack/hosts/releases
   src = pkgs.fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    sha256 = "sha256-Ph0Wc7tl5FHZatSK9BNMZ8lNhGdJKx9X3+Px9MaYKyI=";
+    sha256 = "sha256-pHS/qkAo3T1miZ3OTxi2zLw8r1Nk78wXzsZGKvXa3KA=";
   };
   # Clean up hosts file so that it's only a list of domains. We need to:
   # - Remove some generic host entries for localhost & the like
