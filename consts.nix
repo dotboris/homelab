e7b69db9960de8679fcd281765810a76e0dc90b7
Boris Bera <beraboris@gmail.com>
2025-10-08 17:24:02 -0400

    fix(admin): add laptop ssh key

--- consts.nix
+++ consts.nix
@@ -1,5 +1,8 @@
 {
   dotboris.ssh.pubKeys = [
+    # desktop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSVAavmM8Tz004E0GYZBerdouuquNldm8yozz76ZUw8"
+    # laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9QlBQ++xvislNNi4L2hkYpNHBL6MjrAkVhMRhUrOtg"
   ];
 }
