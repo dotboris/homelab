fa22b5cc676c2692d69bd8c203374433179cac68
Boris Bera <beraboris@gmail.com>
2026-06-27 19:06:01 -0400

    chore: drop last instances of `with lib; ...`

--- modules/backups/destinations/local.nix
+++ modules/backups/destinations/local.nix
@@ -3,49 +3,48 @@
     lib,
     config,
     ...
-  }:
-    with lib; let
-      cfg = config.homelab.backups.destinations.local;
-    in {
-      options.homelab.backups.destinations.local = {
-        enable = mkEnableOption "homelab backend local backend";
-        path = mkOption {
-          type = types.path;
-          default = "/var/lib/homelab-backups/repos/local";
-        };
-        checkAt = lib.mkOption {
-          type = lib.types.nullOr lib.types.str;
-          default = null;
-          description = ''
-            When to check the integrity of the local destination. Integrity
-            checks only run when this is set.
-          '';
+  }: let
+    cfg = config.homelab.backups.destinations.local;
+  in {
+    options.homelab.backups.destinations.local = {
+      enable = lib.mkEnableOption "homelab backend local backend";
+      path = lib.mkOption {
+        type = lib.types.path;
+        default = "/var/lib/homelab-backups/repos/local";
+      };
+      checkAt = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+        description = ''
+          When to check the integrity of the local destination. Integrity
+          checks only run when this is set.
+        '';
+      };
+    };
+    config = lib.mkIf cfg.enable {
+      sops.secrets."backups/repos/local/password" = {
+        owner = "backups";
+      };
+      systemd = {
+        tmpfiles.rules = [
+          "d ${cfg.path} 0700 backups backups"
+        ];
+        timers.backups-check-local = lib.mkIf (cfg.checkAt != null) {
+          wantedBy = ["timers.target"];
+          timerConfig = {
+            Unit = "backups-check@local.service";
+            OnCalendar = cfg.checkAt;
+          };
         };
       };
-      config = mkIf cfg.enable {
-        sops.secrets."backups/repos/local/password" = {
-          owner = "backups";
-        };
-        systemd = {
-          tmpfiles.rules = [
-            "d ${cfg.path} 0700 backups backups"
-          ];
-          timers.backups-check-local = lib.mkIf (cfg.checkAt != null) {
-            wantedBy = ["timers.target"];
-            timerConfig = {
-              Unit = "backups-check@local.service";
-              OnCalendar = cfg.checkAt;
-            };
-          };
-        };
-        services.standard-backups.settings.secrets.localPassword.from-file = config.sops.secrets."backups/repos/local/password".path;
-        homelab.backups._destinations.local.options = {
-          repo = cfg.path;
-          env = {
-            RESTIC_CACHE_DIR = "/var/cache/homelab-backups/restic";
-            RESTIC_PASSWORD = "{{ .Secrets.localPassword }}";
-          };
+      services.standard-backups.settings.secrets.localPassword.from-file = config.sops.secrets."backups/repos/local/password".path;
+      homelab.backups._destinations.local.options = {
+        repo = cfg.path;
+        env = {
+          RESTIC_CACHE_DIR = "/var/cache/homelab-backups/restic";
+          RESTIC_PASSWORD = "{{ .Secrets.localPassword }}";
         };
       };
     };
+  };
 }
