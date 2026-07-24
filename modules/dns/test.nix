b2d2eed21cab8eced2a1cebc905137a0077d0d97
Boris Bera <beraboris@gmail.com>
2026-07-24 17:05:55 -0400

    chore: mark tests as python for nvim

--- modules/dns/test.nix
+++ modules/dns/test.nix
@@ -23,29 +23,31 @@
 
     testScript = {containers, ...}: let
       serverIp = interface: (pkgs.lib.head containers.server.networking.interfaces.${interface}.ipv4.addresses).address;
-    in ''
-      start_all()
-      server.wait_for_unit("coredns.service")
-      server.wait_for_unit("network.target")
-      client.wait_for_unit("network.target")
+    in
+      # python
+      ''
+        start_all()
+        server.wait_for_unit("coredns.service")
+        server.wait_for_unit("network.target")
+        client.wait_for_unit("network.target")
 
-      with subtest("internal ips (lan)"):
-        assert "10.0.42.2" in client.succeed("nslookup homelab.lan ${serverIp "eth1"}")
-        assert "10.0.42.2" in client.succeed("nslookup home.dotboris.io ${serverIp "eth1"}")
-        assert "10.0.42.3" in client.succeed("nslookup homelab-test.lan ${serverIp "eth1"}")
-        assert "10.0.42.3" in client.succeed("nslookup home-test.dotboris.io ${serverIp "eth1"}")
+        with subtest("internal ips (lan)"):
+          assert "10.0.42.2" in client.succeed("nslookup homelab.lan ${serverIp "eth1"}")
+          assert "10.0.42.2" in client.succeed("nslookup home.dotboris.io ${serverIp "eth1"}")
+          assert "10.0.42.3" in client.succeed("nslookup homelab-test.lan ${serverIp "eth1"}")
+          assert "10.0.42.3" in client.succeed("nslookup home-test.dotboris.io ${serverIp "eth1"}")
 
-      with subtest("internal ips (tailscale)"):
-        assert "100.69.230.33" in client.succeed("nslookup homelab.lan ${serverIp "eth2"}")
-        assert "100.69.230.33" in client.succeed("nslookup home.dotboris.io ${serverIp "eth2"}")
-        assert "100.67.226.105" in client.succeed("nslookup homelab-test.lan ${serverIp "eth2"}")
-        assert "100.67.226.105" in client.succeed("nslookup home-test.dotboris.io ${serverIp "eth2"}")
+        with subtest("internal ips (tailscale)"):
+          assert "100.69.230.33" in client.succeed("nslookup homelab.lan ${serverIp "eth2"}")
+          assert "100.69.230.33" in client.succeed("nslookup home.dotboris.io ${serverIp "eth2"}")
+          assert "100.67.226.105" in client.succeed("nslookup homelab-test.lan ${serverIp "eth2"}")
+          assert "100.67.226.105" in client.succeed("nslookup home-test.dotboris.io ${serverIp "eth2"}")
 
-      with subtest("adblock (lan)"):
-        assert "NXDOMAIN" in client.fail("nslookup doubleclick.net ${serverIp "eth1"}")
+        with subtest("adblock (lan)"):
+          assert "NXDOMAIN" in client.fail("nslookup doubleclick.net ${serverIp "eth1"}")
 
-      with subtest("adblock (tailscale)"):
-        assert "NXDOMAIN" in client.fail("nslookup doubleclick.net ${serverIp "eth2"}")
-    '';
+        with subtest("adblock (tailscale)"):
+          assert "NXDOMAIN" in client.fail("nslookup doubleclick.net ${serverIp "eth2"}")
+      '';
   };
 }
--- modules/standard-backups/test.nix
+++ modules/standard-backups/test.nix
@@ -44,46 +44,48 @@
         "d /var/lib/restore/ 0700 root root"
       ];
     };
-    testScript = {...}: ''
-      start_all()
-      machine.succeed("standard-backups print-config")
-      machine.succeed("standard-backups validate-config")
+    testScript =
+      # python
+      ''
+        start_all()
+        machine.succeed("standard-backups print-config")
+        machine.succeed("standard-backups validate-config")
 
-      with subtest("cli"):
-        machine.succeed("echo hello from cli > /var/lib/back-me-up/file.txt")
-        machine.succeed("standard-backups backup test")
+        with subtest("cli"):
+          machine.succeed("echo hello from cli > /var/lib/back-me-up/file.txt")
+          machine.succeed("standard-backups backup test")
 
-        # restore restic
-        machine.succeed("""
-          standard-backups exec -d restic-test -- \
-            restore latest:/var/lib/back-me-up/ \
-            -t /var/lib/restore/restic-cli/
-        """)
-        assert "hello from cli" in machine.succeed("cat /var/lib/restore/restic-cli/file.txt")
+          # restore restic
+          machine.succeed("""
+            standard-backups exec -d restic-test -- \
+              restore latest:/var/lib/back-me-up/ \
+              -t /var/lib/restore/restic-cli/
+          """)
+          assert "hello from cli" in machine.succeed("cat /var/lib/restore/restic-cli/file.txt")
 
-        # check rsync
-        assert "hello from cli" in machine.succeed("""
-          file="$(find /var/lib/backups/rsync-test -name file.txt | sort | tail -n1)"
-          cat "$file"
-        """)
+          # check rsync
+          assert "hello from cli" in machine.succeed("""
+            file="$(find /var/lib/backups/rsync-test -name file.txt | sort | tail -n1)"
+            cat "$file"
+          """)
 
-      with subtest("service"):
-        machine.succeed("echo hello from service > /var/lib/back-me-up/file.txt")
-        machine.start_job("standard-backups@test")
+        with subtest("service"):
+          machine.succeed("echo hello from service > /var/lib/back-me-up/file.txt")
+          machine.start_job("standard-backups@test")
 
-        # restore rsync
-        machine.succeed("""
-          standard-backups exec -d restic-test -- \
-            restore latest:/var/lib/back-me-up/ \
-            -t /var/lib/restore/restic-service/
-        """)
-        assert "hello from service" in machine.succeed("cat /var/lib/restore/restic-service/file.txt")
+          # restore rsync
+          machine.succeed("""
+            standard-backups exec -d restic-test -- \
+              restore latest:/var/lib/back-me-up/ \
+              -t /var/lib/restore/restic-service/
+          """)
+          assert "hello from service" in machine.succeed("cat /var/lib/restore/restic-service/file.txt")
 
-        # check rsync
-        assert "hello from service" in machine.succeed("""
-          file="$(find /var/lib/backups/rsync-test -name file.txt | sort | tail -n1)"
-          cat "$file"
-        """)
-    '';
+          # check rsync
+          assert "hello from service" in machine.succeed("""
+            file="$(find /var/lib/backups/rsync-test -name file.txt | sort | tail -n1)"
+            cat "$file"
+          """)
+      '';
   });
 }
